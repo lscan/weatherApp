a42a1cb14ad9894145313df2f08d7082e5c2ce5e
Lee <lelandscanlan@gmail.com>
2016-06-27 21:46:20 -0400

    temp scales working

--- main.js
+++ main.js
@@ -1,25 +1,12 @@
-/*
+// todo:
+// remove the old chart when you search for a new one
 
-- Sign up for openweathermap.org and generate an API key.
-- User either $.ajax or $.get to pull weather current data .
-  for Washington DC (hint: http://api.openweathermap.org/data/2.5/weather?q=...).
-- Print the temperature in console.
-- Bonus 1: add a form prompting user for the city and state.
-- Bonus 2: convert answer from kelvin to fahrenheit.
-
-*/
-
-// var responseData;
-'use strict';
+// chart stuff
 var ctx;
 var ctxData;
 var ctxOptions;
-// require(['node_modules/core-js/client/core.js'], function () {});
-require(['node_modules/chart.js/dist/Chart.js'], function(){
+require(['node_modules/chart.js/dist/Chart.js'], function() {
   ctx = document.getElementById("myChart");
-
-  //trying line chart
-
   ctxData = {
     labels: [],
     datasets: [
@@ -37,67 +24,74 @@ require(['node_modules/chart.js/dist/Chart.js'], function(){
   ctxOptions = {
     showLines: true
   }
-
-
 });
 
-
-
-
-var currentLocationButton
+var currentLocationButton;
 var response;
 var weatherApp = {};
+
 (function() {
   document.getElementById('city-click').addEventListener('click', function() {
-    document.getElementsByClassName('city-search')[0].style.display = "none";
-    document.getElementsByClassName('location-search')[0].style.display = "none";
-    document.getElementsByClassName('zip-search')[0].style.display = "block";
-  });
-  document.getElementById('zip-click').addEventListener('click', function() {
-    document.getElementsByClassName('zip-search')[0].style.display = "none";
-    document.getElementsByClassName('location-search')[0].style.display = "none";
+    hideSections();
     document.getElementsByClassName('city-search')[0].style.display = "block";
   });
+  document.getElementById('zip-click').addEventListener('click', function() {
+    hideSections();
+    document.getElementsByClassName('zip-search')[0].style.display = "block";
+  });
   document.getElementById('location-click').addEventListener('click', function() {
-    document.getElementsByClassName('city-search')[0].style.display = "none";
-    document.getElementsByClassName('zip-search')[0].style.display = "none";
+    hideSections();
     document.getElementsByClassName('location-search')[0].style.display = "block";
   });
+  function hideSections() {
+    document.getElementsByClassName('city-search')[0].style.display = "none";
+    document.getElementsByClassName('zip-search')[0].style.display = "none";
+    document.getElementsByClassName('location-search')[0].style.display = "none";
+  }
   weatherApp.baseUrl = "http://api.openweathermap.org/data/2.5/weather?";
   weatherApp.baseUrlForecast = "http://api.openweathermap.org/data/2.5/forecast?";
   weatherApp.apiKey = "apikey=33b2850b60fc24e3dd76cae396d00544";
   weatherApp.zip = "&zip=";
   weatherApp.city = "&q=";
   weatherApp.fiveDay = false;
+  weatherApp.tempScale = "fahrenheit";
   var httpRequest;
 
+  // for getting temp by zip code
   document.getElementById("zip-submit").onclick = function(e) {
     e.preventDefault();
-    console.log('Making request by zip code.');
-    var newZip = weatherApp.zip + document.getElementById('zip-value').value;
-    $.get(weatherApp.baseUrl+weatherApp.apiKey+newZip, function( data ) {
-  		document.getElementById('zipOrCity').innerHTML = document.getElementById('zip-value').value;
-    	document.getElementById('temperature').innerHTML = convertKelvin(data.main.temp);
+    var thisZip = document.getElementById('zip-value').value;
+    $.get(weatherApp.baseUrl + weatherApp.apiKey + "&zip=" + thisZip, function( data ) {
+  		document.getElementById('zipOrCity').innerHTML = thisZip;
+      getWeatherByTempScale(data);
+      setTempScaleInDom();
     	document.getElementById('result').style.display = "block";
-    	response = data;
     	document.getElementById('zip-value').value = "";
 		});
-  }
-  document.getElementById("city-submit").onclick = function(e) {
-  	e.preventDefault();
-  	console.log('Making request by city.');
-  	var newCity = weatherApp.city + document.getElementById('city-value').value;
-  	$.get(weatherApp.baseUrl+weatherApp.apiKey+newCity, function( data ) {
-  		document.getElementById('zipOrCity').innerHTML = document.getElementById('city-value').value;
-    	document.getElementById('temperature').innerHTML = convertKelvin(data.main.temp);
-    	document.getElementById('result').style.display = "block";
-    	response = data;
-    	document.getElementById('city-value').value = "";
-		});
     if(weatherApp.fiveDay == true) {
-      fiveDayCity();
+      fiveDay("zip", null, thisZip, null, null);
     }
   }
+
+  // for getting temp by city
+  document.getElementById("city-submit").onclick = function(e) {
+  	e.preventDefault();
+    var thisCity = document.getElementById('city-value').value;
+  	$.get(weatherApp.baseUrl + weatherApp.apiKey + "&q=" + thisCity, function( data ) {
+  		document.getElementById('zipOrCity').innerHTML = thisCity;
+      getWeatherByTempScale(data);
+      setTempScaleInDom();
+    	document.getElementById('result').style.display = "block";
+    	document.getElementById('city-value').value = "";
+		});
+    // this removes a potential space in between the city and state, which won't work with the API call
+    thisCity.replace(' ', '');
+    if(weatherApp.fiveDay == true) {
+      fiveDay("city", thisCity, null, null, null);
+    }
+  }
+
+  // for getting temp by current lat/lon
   document.getElementById("location-submit").onclick = function(e) {
     e.preventDefault();
     if(navigator.geolocation) {
@@ -111,19 +105,27 @@ var weatherApp = {};
           success: function(response) {
             var data = response;
             document.getElementById('zipOrCity').innerHTML = "lat " + weatherApp.lat.toFixed(2) + ", lon " + weatherApp.lon.toFixed(2);
-            document.getElementById('temperature').innerHTML = convertKelvin(data.main.temp);
+            getWeatherByTempScale(data);
+            setTempScaleInDom();
             document.getElementById('result').style.display = "block";
             if(weatherApp.fiveDay == true) {
-              fiveDayCity();
+              fiveDay("currentLocation", null, null, weatherApp.lat, weatherApp.lon);
             }
           }
         });
       }, function(error){console.log(error)});
-    } else {console.log('nah');}
+    } else {console.log('no navigator.geolocation');}
   }
-  function convertKelvin(kelvinTemp) {
-  	return Math.round((kelvinTemp*(9/5)-459.67)*10)/10;
+
+  // conversion functions
+  function convertKelvinToFahrenheit(kelvinTemp) {
+    return Math.round((kelvinTemp*(9/5)-459.67)*10)/10;
   }
+  function convertKelvinToCelsius(kelvinTemp) {
+    return Math.round(kelvinTemp - 273.15);
+  }
+
+  // function to get temp by lat/lon
   function currentLocation() {
     var currentLocationEndpoint = weatherApp.baseUrl + "lat=" + weatherApp.lat + "&lon=" + weatherApp.lon + "&" + weatherApp.apiKey;
     $.ajax({
@@ -136,10 +138,17 @@ var weatherApp = {};
     });
   }
 
-  // five day forecasts
-  function fiveDayCity() {
-    var newCity = weatherApp.city + document.getElementById('city-value').value;
-    var endpoint = weatherApp.baseUrlForecast + newCity + "&" + weatherApp.apiKey;
+  // five day forecasts function
+  // type is zip, city, or lat/lon
+  function fiveDay(type, city, zip, lat, lon) {
+    var endpoint = weatherApp.baseUrlForecast;
+    if(type == "city") {
+      endpoint = endpoint + "q=" + city + "&" + weatherApp.apiKey;
+    } else if(type == "zip") {
+      endpoint = endpoint + "zip=" + zip + "&" + weatherApp.apiKey;
+    } else if(type == "currentLocation") {
+      endpoint = endpoint + "lat=" + lat.toFixed(2) + "&lon=" + lon.toFixed(2) + "&" + weatherApp.apiKey;
+    }
     $.ajax({
       url: endpoint,
       method: 'GET',
@@ -149,12 +158,21 @@ var weatherApp = {};
         for(var i=0; i<response.list.length; i++) {
           //grab data
           var fiveDayDatetime = response.list[i].dt_txt;
-          var fiveDayTemp = convertKelvin(response.list[i].main.temp);
-          var fiveDayHumidity = response.list[i].main.humidity;
-          var fiveDayCloudiness = response.list[i].clouds.all;
-          var fiveDayWind = response.list[i].wind.speed;
-          var fiveDayWeatherDescription = response.list[i].weather[0].description;
-          var fiveDayWeatherMain = response.list[i].weather[0].main;
+          var fiveDayTemp;
+          
+          if(weatherApp.tempScale == "fahrenheit") {
+            fiveDayTemp = convertKelvinToFahrenheit(response.list[i].main.temp);
+          } else if(weatherApp.tempScale == "celsius") {
+            fiveDayTemp = convertKelvinToCelsius(response.list[i].main.temp);
+          } else if(weatherApp.tempScale == "kelvin") {
+            fiveDayTemp = response.list[i].main.temp;
+          }
+
+          // var fiveDayHumidity = response.list[i].main.humidity;
+          // var fiveDayCloudiness = response.list[i].clouds.all;
+          // var fiveDayWind = response.list[i].wind.speed;
+          // var fiveDayWeatherDescription = response.list[i].weather[0].description;
+          // var fiveDayWeatherMain = response.list[i].weather[0].main;
 
           // for chart
           ctxData.labels.push(fiveDayDatetime);
@@ -166,21 +184,59 @@ var weatherApp = {};
             data: ctxData,
             options: ctxOptions
           });
-      }
+        }
     });
   }
-  //function for determining if button is checked or unchecked
+  // determine if button is checked or unchecked
   function fiveDayClick() {
     if(fiveDayButton.checked == true) {
-      console.log('set weatherApp.fiveDay to true');
       weatherApp.fiveDay = true;
     } else if(fiveDayButton.checked == false) {
-      console.log('set weatherApp.fiveDay to false');
       weatherApp.fiveDay = false;
     }
   }
+  // set weatherApp.tempScale to fahrenheit, celsius, or kelvin
+  function setTempScale() {
+    if(document.getElementById('fahrenheit').checked == true) {
+      weatherApp.tempScale = "fahrenheit";
+    } else if(document.getElementById('celsius').checked == true) {
+      weatherApp.tempScale = "celsius";
+    } else if(document.getElementById('kelvin').checked == true) {
+      weatherApp.tempScale = "kelvin";
+    }
+  }
+  // create the wording on the page
+  function setTempScaleInDom() {
+    var tempScaleSpan = document.getElementById('temp-scale');
+    if(weatherApp.tempScale == "fahrenheit") {
+      tempScaleSpan.innerHTML = "Fahrenheit";
+    } else if(weatherApp.tempScale == "celsius") {
+      tempScaleSpan.innerHTML = "Celsius";
+    } else if(weatherApp.tempScale == "kelvin") {
+      tempScaleSpan.innerHTML = "Kelvin";
+    }
+  }
+  // when actually getting the data from the API call, this determines what to convert the temp to
+  function getWeatherByTempScale(data) {
+    if(weatherApp.tempScale == "fahrenheit") {
+      document.getElementById('temperature').innerHTML = convertKelvinToFahrenheit(data.main.temp);
+    } else if(weatherApp.tempScale == "celsius") {
+      document.getElementById('temperature').innerHTML = convertKelvinToCelsius(data.main.temp);
+    } else if(weatherApp.tempScale == "kelvin") {
+      document.getElementById('temperature').innerHTML = data.main.temp;
+    }
+  }
+
+  // listener for the button that determines whether the 5-day forecast should be displayed
   var fiveDayButton = document.getElementById('cb4');
-  fiveDayButton.addEventListener('click', fiveDayClick)
+  fiveDayButton.addEventListener('click', fiveDayClick);
+  // add the listeners to the radio buttons
+  var tempScaleRadios = document.getElementsByName('r-group-1');
+  for(var i=0; i<tempScaleRadios.length; i++) {
+    tempScaleRadios[i].addEventListener('click', setTempScale);
+  }
+  // listener for the current location button
   currentLocationButton = document.getElementById('current');
   currentLocationButton.addEventListener('click', currentLocation);
+
 })();
